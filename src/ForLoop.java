820fc80d9383969cabf3494ec8a3c07488da6da9
DimaKrempoviy <krempoviy98@gmail.com>
2021-11-15 17:13:50 +0200

    commit message

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,5 +1,7 @@
 public class ForLoop {
     public static void main(String[] args) {
-        
+        for ( int i = 10; i >= 0; i = i - 5){                   //i++ == i = i +1 (true)
+            System.out.println("Hello " +i);
+        }
     }
 }
