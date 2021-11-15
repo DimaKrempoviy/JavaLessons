a7e73ef51b70957c4a19db125ab3f380c250034d
DimaKrempoviy <krempoviy98@gmail.com>
2021-11-15 16:16:38 +0200

    first lessons

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 557;
+        short myShort = 3266;
+        long mtLong = 123123123;
+
+        double myDouble = 234.12;
+        float myFloat = 21.1f;
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 120; //128 -128
+        System.out.println(myInt);
+    }
+}
