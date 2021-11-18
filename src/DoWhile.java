5feec4c1b8f6c7101fafd4858dfe751dad5ae35d
DimaKrempoviy <krempoviy98@gmail.com>
2021-11-18 16:59:46 +0200

    lessons 4 day

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -7,7 +7,7 @@ public class DoWhile {
         do {
             System.out.println("Введи 5");
             value = scanner.nextInt();
-        } while (value !=5);
+        } while (value != 5);
         System.out.println("Вы ввели 5");
 
     }
