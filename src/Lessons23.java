d64e3e7e5fff064f0affe2682a0f639d2d90cb84
DimaKrempoviy <krempoviy98@gmail.com>
2021-11-29 17:02:49 +0200

    Lessons 29.11

--- src/Lessons23.java
+++ src/Lessons23.java
@@ -1,5 +1,9 @@
 public class Lessons23 {
     public static void main(String[] args) {
-
+        System.out.printf("String %.2f \n",12.2234521);
+        System.out.printf("String %.2f \n",12.21);
+        System.out.printf("String %.2f \n",12.223523523521);
+        System.out.printf("String %.2f \n",12.2235235235235231);
+        // % -[10][.42]
     }
 }
