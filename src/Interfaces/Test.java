cf6144750e5f4ede7704da038b4b6c6569a1f692
DimaKrempoviy <krempoviy98@gmail.com>
2021-12-03 17:18:05 +0200

    lessons 03.12

---
+++ src/Interfaces/Test.java
@@ -0,0 +1,13 @@
+package Interfaces;
+
+public class Test {
+    public static void main(String[] args) {
+        Info info1 = new Animal(1);
+        Info info2 = new Person("Bob");
+        outputInfo(info1);
+        outputInfo(info2);
+    }
+    public static void outputInfo(Info info){
+        info.showInfo();
+    }
+}
---
+++ src/Lessons24.java
@@ -0,0 +1,5 @@
+public class Lessons24 {
+    public static void main(String[] args) {
+
+    }
+}
