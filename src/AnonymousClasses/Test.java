94a08aec60e2ce552c69c4d3e8d6eaeb39867fd9
DimaKrempoviy <krempoviy98@gmail.com>
2021-12-10 16:10:54 +0200

    10.12

---
+++ src/AnonymousClasses/Test.java
@@ -0,0 +1,17 @@
+package AnonymousClasses;
+
+interface AbleToEat {
+    public void eat();
+}
+
+public class Test {
+    public static void main(String[] args) {
+        AbleToEat ableToEat = new AbleToEat() {
+            @Override
+            public void eat() {
+                System.out.println("Some is eating...");
+            }
+        };
+        ableToEat.eat();
+    }
+}
